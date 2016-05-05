5bdd9117348da4e517d9be4d17541e61052d4f5b
CatWantsMeow <cat.wants.meow@gmail.com>
2016-05-05 13:47:53 +0300

    Implemented algotithm for solving transhipment problem

--- 4th_lab/transportation_problem.py
+++ 4th_lab/transportation_problem.py
@@ -1,81 +1,182 @@
-from copy import copy, deepcopy
+# coding=utf-8
+from numpy import array, zeros
+from numpy.linalg import solve
 
 
-class Math(object):
-
-    @staticmethod
-    def zeros(n, m):
-        """ Generate nxm dimensions array filled by zeros """
-        array = []
-        for i in xrange(n):
-            array.append([0 for j in xrange(m)])
-        return array
-
-    @staticmethod
-    def pmin(*args):
-        """ Get minimal positive value from passed arguments """
-        return max(min(*args), 0)
-
-    @staticmethod
-    def prettify(array):
-        """ Get pretty string interpretation of two-dimensional array """
-        return '\n'.join(''.join("{:>4}".format(elem) for elem in row) for row in array)
-
-
-class TranshipmentProblem(object):
-
-    eps = 0.00001
-
-    def __init__(self, supply, demand, cost):
-        self.supply = deepcopy(supply)
-        self.demand = deepcopy(demand)
-        self.cost = deepcopy(cost)
-
-    def _balance(self):
-        """ Added additional value of supply or demand if sum of supply and demand is not equal """
-        total_supply = sum(self.supply)
-        total_demand = sum(self.demand)
-        if total_supply > total_demand:
-            self.demand.append(total_supply - total_demand)
-        elif total_demand > total_supply:
-            self.supply.append(total_demand - total_supply)
-
-    def _prevent_degeneracy(self):
-        """Add a small amount to demand and total supply to prevent degeneracy"""
-        self.demand = [d + self.eps for d in self.demand]
-        self.supply[0] += len(self.demand) * self.eps
-
-    def _get_initial_solution(self):
-        """ Calculation of initial solution by north-west algorithm """
-        supply, demand = copy(self.supply), copy(self.demand)
-        amounts = Math.zeros(len(supply), len(demand))
-        for i, provider in enumerate(supply):
-            for j, consumer in enumerate(demand):
-                if supply[i] <= 0:
-                    break
-                amounts[i][j] = Math.pmin(supply[i], demand[j])
-                supply[i] -= amounts[i][j]
-                demand[j] -= amounts[i][j]
-        return amounts
+def south_west_angle_method(supply, demand):
+    i, j = 0, 0
+    supply, demand = array(supply), array(demand)
+    basis_coords = list()
+    amounts = zeros([len(supply), len(demand)])
+    while i != len(supply) and j != len(demand):
+        basis_coords.append((i, j))
+        if demand[j] < supply[i]:
+            amounts[i, j] = demand[j]
+            supply[i] -= demand[j]
+            j += 1
+        elif supply[i] < demand[j]:
+            amounts[i, j] = supply[i]
+            demand[j] -= supply[i]
+            i += 1
+        else:
+            amounts[i, j] = supply[i]
+            i += 1
+            if i == len(supply):
+                break
+            basis_coords.append((i, j))
+            j += 1
+    return amounts, array(basis_coords)
 
 
-    def solve():
-        pass
+def get_neighbours(amounts, i, j, basis_coords, is_new=False):
+    neighbours = list()
+
+    l = j
+    k = i - 1
+    while k >= 0:
+        if (k, l) in basis_coords:
+            neighbours.append((k, l))
+            break
+        k -= 1
+
+    k = i
+    l = j - 1
+    while l >= 0:
+        if (k, l) in basis_coords:
+            neighbours.append((k, l))
+            break
+        l -= 1
+    k = i + 1
+    l = j
+
+    if is_new and len(neighbours) == 2:
+        return neighbours
+
+    while k < amounts.shape[0]:
+        if (k, l) in basis_coords:
+            neighbours.append((k, l))
+            break
+        k += 1
+    k = i
+    l = j + 1
+
+    if is_new and len(neighbours) == 2:
+        return neighbours
+    while l < amounts.shape[1]:
+        if (k, l) in basis_coords:
+            neighbours.append((k, l))
+            break
+        l += 1
+    return neighbours
+
+
+def get_corner_peaks(path):
+    corner_peaks = list()
+    length = len(path)
+    for k, peak in enumerate(path):
+        i = (k + 1) % length
+        j = (k - 1 + length) % length
+        if path[i][0] != path[j][0] and path[i][1] != path[j][1]:
+            corner_peaks.append(peak)
+    return corner_peaks
+
+
+def dfs(v, ancestors, adjacency_list):
+    for neighbour in adjacency_list[v]:
+        if neighbour == ancestors[v]:
+            continue
+        else:
+            ancestors[neighbour] = v
+            dfs(neighbour, ancestors, adjacency_list)
+
+
+def get_cycle(amounts, basis_coords, i0, j0):
+    coords_tuple = tuple(tuple(coord) for coord in basis_coords)
+    ancestors = {k: [] for k in coords_tuple}
+
+    adjacency_list = dict()
+    adjacency_list[i0, j0] = get_neighbours(amounts, i0, j0, coords_tuple, True)
+    for i in xrange(amounts.shape[0]):
+        for j in xrange(amounts.shape[1]):
+            if (i, j) in coords_tuple:
+                adjacency_list[(i, j)] = get_neighbours(amounts, i, j, coords_tuple)
+
+    dfs(adjacency_list[(i0, j0)][0], ancestors, adjacency_list)
+
+    path = list()
+    start = adjacency_list[(i0, j0)][0]
+    current = adjacency_list[(i0, j0)][1]
+    while current != start:
+        path.append(current)
+        current = ancestors[current]
+    path.append(start)
+    path.insert(0, (i0, j0))
+    return get_corner_peaks(path)
+
+
+def matrix_transport_problem(amounts, basis_coords, costs):
+    while True:
+        equations = zeros([basis_coords.shape[0] + 1, costs.shape[0] + costs.shape[1]])
+        b = zeros(basis_coords.shape[0] + 1)
+        for k, (i, j) in enumerate(basis_coords):
+            equations[k, i], equations[k, j + costs.shape[0]] = 1, 1
+            b[k] = costs[i, j]
+        equations[basis_coords.shape[0], 0] = 1
+        b[basis_coords.shape[0]] = 0
+
+        results = solve(equations, b)
+        u, v = results[:costs.shape[0]], results[costs.shape[0]:]
+
+        coords_tuple = tuple(tuple(coord) for coord in basis_coords)
+        non_basis_coords = [
+            (i, j)
+            for i in xrange(costs.shape[0])
+            for j in xrange(costs.shape[1])
+            if (i, j) not in coords_tuple
+        ]
+
+        if all(costs[i, j] > u[i] + v[j] for i, j in non_basis_coords):
+            return amounts, basis_coords
+
+        old_coord = min(filter(lambda (i, j): u[i] + v[j] > costs[i, j], non_basis_coords))
+        cycle = get_cycle(amounts, basis_coords, *old_coord)
+        marks = {'+': cycle[::2], '-': cycle[1::2]}
+
+        min_coords = array([amounts[i, j] for i, j in marks['-']])
+        theta = min_coords.min()
+        new_coord = marks['-'][min_coords.argmin()]
+
+        for i, j in cycle:
+            amounts[i, j] += theta if (i, j) in marks['+'] else -theta
+
+        basis_coords = list([tuple(coord) for coord in basis_coords])
+        basis_coords.append(old_coord)
+        basis_coords.remove(new_coord)
+        basis_coords = array(basis_coords)
 
 
 def main():
-    supply = [20, 30, 25]
-    demand = [10, 10, 10, 10, 10]
-    cost = [
-        [2, 8, -5, 7, 10],
-        [11, 5, 8, -8, 4],
-        [1, 3, 7, 4, 2],
-    ]
+    supply = array([15, 12, 18, 20])
+    demand = array([5, 5, 10, 4, 6, 20, 10, 5])
+    cost = array([
+        [-3, 10, 70, -3, 7, 4, 2, -20],
+        [3, 5, 8, 8, 0, 1, 7, -10],
+        [-15, 1, 0, 0, 13, 5, 4, 5],
+        [1, -5, 9, -3, -4, 7, 16, 25],
+    ])
 
-    TranshipmentProblem._balance(supply, demand)
-    amounts = TranshipmentProblem._get_initial_solution(supply, demand)
-    print TranshipmentProblem._check_consistence(amounts)
+    initial_amounts, basis_coords = south_west_angle_method(supply, demand)
+    optimal_amounts, basis_coords = matrix_transport_problem(initial_amounts, basis_coords, cost)
+
+    transhipment_costs = 0
+    for i, row in enumerate(optimal_amounts):
+        for j, elem in enumerate(row):
+            transhipment_costs += cost[i, j] * optimal_amounts[i, j]
+
+    pretty_amounts = "\n".join("".join("{:<8}".format(elem) for elem in row) for row in optimal_amounts)
+    print "Оптимальный план перевозок:\n", pretty_amounts
+    print "Затраты на перевозку:", transhipment_costs
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
